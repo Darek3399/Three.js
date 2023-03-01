c4304bebc9dc72806d8ac219d1bbab611b33685e
ManSIayeR <103418605+Darek3399@users.noreply.github.com>
2023-03-02 04:12:45 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,226 @@
+import * as THREE from 'three'
+import * as CANNON from 'cannon-es'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// скайбокс
+import sky3Box0 from '../src/img/sky_boxes/city/skybox0.jpg'
+import sky3Box1 from '../src/img/sky_boxes/city/skyBox1.jpg'
+import sky3Box2 from '../src/img/sky_boxes/city/skyBox2.jpg'
+import sky3Box3 from '../src/img/sky_boxes/city/skyBox3.jpg'
+import sky3Box4 from '../src/img/sky_boxes/city/skyBox4.jpg'
+import sky3Box5 from '../src/img/sky_boxes/city/skyBox5.jpg'
+
+
+
+
+
+
+
+
+
+
+// рендер
+const renderer = new THREE.WebGLRenderer({
+	antialias: true
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement)
+
+// сцена
+const scene = new THREE.Scene()
+
+// камера
+const camera = new THREE.PerspectiveCamera(
+	45,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	40000
+)
+camera.position.set(5, 10, 15)
+
+
+const orbit = new OrbitControls(camera, renderer.domElement)
+orbit.update()
+
+
+// скайбокс
+const skyBox = new THREE.CubeTextureLoader()
+scene.background = skyBox.load([
+	sky3Box2,
+	sky3Box4,
+	sky3Box1, //верх
+	sky3Box0, //низ
+	sky3Box5,
+	sky3Box3,
+])
+
+
+
+
+
+
+// ФИЗИКА===========================================================================
+// =================================================================================
+const world = new CANNON.World({
+	gravity: new CANNON.Vec3(0, -9.81, 0)
+})
+const timeStep = 1 / 60
+
+
+
+
+
+
+// ОБЪЕКТЫ
+
+
+
+// поверхность
+
+const groundGeo = new THREE.PlaneGeometry(50, 50)
+const groundMat = new THREE.MeshStandardMaterial({
+	color: 0x111919,
+	side: THREE.DoubleSide,
+	roughness: 0.5,
+	metalness: 0.5,
+})
+const ground = new THREE.Mesh(groundGeo, groundMat)
+ground.receiveShadow = true
+ground.position.set(0, 0, 0)
+scene.add(ground)
+
+
+
+// коллизия ground
+const groundPhisMat = new CANNON.Body()
+const groundBody = new CANNON.Body({
+	shape: new CANNON.Box(new CANNON.Vec3(ground.geometry.parameters.width / 2, ground.geometry.parameters.height / 2, 0.01)),
+	mass: 0,
+	material: groundPhisMat,
+})
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
+world.addBody(groundBody)
+
+
+
+
+
+
+
+
+
+
+
+// бокс
+const boxGeo = new THREE.BoxGeometry()
+const boxMat = new THREE.MeshStandardMaterial({
+	color: 0xf8db5
+})
+const box = new THREE.Mesh(boxGeo, boxMat)
+box.castShadow = true
+box.position.set(0, 2, 0)
+scene.add(box)
+
+
+// ширина/высота/глубина куба для CANNON
+const boxDHW = [box.geometry.parameters.depth / 2, box.geometry.parameters.height / 2, box.geometry.parameters.width / 2]
+
+// // коллизия box
+const boxPhisMat = new CANNON.Material()
+const boxBody = new CANNON.Body({
+	mass: 1,
+	shape: new CANNON.Box(new CANNON.Vec3(boxDHW[0], boxDHW[1], boxDHW[2])),
+	position: new CANNON.Vec3(0, 10, 0),
+	material: boxPhisMat,
+})
+console.log(boxBody)
+// boxBody.linearDamping = 0.02 //сопротимвление воздуха для перемещения
+boxBody.angularDamping = 0.5 //сопротимвление воздуха для раскручивания
+world.addBody(boxBody)
+
+// взаимодействие ground и box
+const groBoxContactMat = new CANNON.ContactMaterial(
+	groundPhisMat,
+	boxPhisMat,
+	{
+		friction: 0,
+	}
+)
+scene.add(box)
+world.addContactMaterial(groBoxContactMat)
+
+
+
+
+
+
+
+
+// СВЕТ
+
+const dLight = new THREE.DirectionalLight(0xFFFFFF, 3)
+scene.add(dLight)
+dLight.position.set(55, 250, 170)
+dLight.castShadow = true
+dLight.shadow.camera.top = 25
+dLight.shadow.camera.right = 25
+dLight.shadow.camera.bottom = -25
+dLight.shadow.camera.left = -25
+dLight.shadow.mapSize.x = 8192
+dLight.shadow.mapSize.y = 8192
+
+
+
+
+
+
+
+
+
+
+
+
+
+// счётчик FPS
+let countFPS = 0
+setInterval(() => {
+	document.querySelector(`#fps`).innerHTML = countFPS
+	countFPS = 0
+}, 1000)
+
+
+
+
+
+// АНИМАЦИЯ
+const animate = () => {
+	// счётчик FPS
+	countFPS += 1
+
+	// ФИЗИКА
+	ground.position.copy(groundBody.position)
+	ground.quaternion.copy(groundBody.quaternion)
+
+	
+	box.position.copy(boxBody.position)
+	box.quaternion.copy(boxBody.quaternion)
+
+
+
+
+	world.step(timeStep)
+	renderer.render(scene, camera)
+}
+renderer.setAnimationLoop(animate)
+
+
+
+
+// ресайз окна
+window.onresize = () => {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+}
