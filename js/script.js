fa6d1ad58662e1fb243f1f0e8c65b9d7b8ffd37c
ManSIayeR <103418605+Darek3399@users.noreply.github.com>
2023-03-03 04:44:11 +0500

    tryh

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import * as CANNON from 'cannon-es'
 
-const skyBox0 = '../src/img/sky_boxes/city/skyBox0.jpg'
-const skyBox1 = '../src/img/sky_boxes/city/skyBox1.jpg'
-const skyBox2 = '../src/img/sky_boxes/city/skyBox2.jpg'
-const skyBox3 = '../src/img/sky_boxes/city/skyBox3.jpg'
-const skyBox4 = '../src/img/sky_boxes/city/skyBox4.jpg'
-const skyBox5 = '../src/img/sky_boxes/city/skyBox5.jpg'
+const skyBox0 = './src/img/sky_boxes/city/skyBox0.jpg'
+const skyBox1 = './src/img/sky_boxes/city/skyBox1.jpg'
+const skyBox2 = './src/img/sky_boxes/city/skyBox2.jpg'
+const skyBox3 = './src/img/sky_boxes/city/skyBox3.jpg'
+const skyBox4 = './src/img/sky_boxes/city/skyBox4.jpg'
+const skyBox5 = './src/img/sky_boxes/city/skyBox5.jpg'
 
 
 
